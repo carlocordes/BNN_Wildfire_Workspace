7b13b7642b6eb18120fd67e6ea4b729e8e56a444
Carlo <carlo@familiecordes.de>
2026-02-10 16:38:37 +0100

    Initial class-based structure

--- code/ViT.py
+++ code/ViT.py
@@ -2,22 +2,119 @@ import torch
 import torch.nn as nn
 
 class STViT(nn.Module):
+    """
+    Spatio-temporal Vision Transformer
+        Args:
+        batch_size:
+        num_modules:
+        patch_size:
+        embedding_dim : 
+    """
     def __init__(self,
                  batch_size : int,
                  num_modules : int,
                  patch_size : int,
-                 ):
+                 img_height : int,
+                 img_width : int,
+                 embedding_dim: int,
+        ):
+        
         super().__init__()
+        
+        #Parameters
+        self.batch_size = batch_size
+        self.num_modules = num_modules
+        self.patch_size = patch_size
+        self.img_height = img_height
+        self.img_width = img_width
+        self.embedding_dim = embedding_dim
+
+        self.num_patches_per_image = int((img_height / patch_size) * (img_width / patch_size))
+        
+        # Network Classes
+        self.patch_embedding = nn.Linear(patch_size*patch_size, embedding_dim)
+        self.pos_encoding = nn.Parameter(
+            torch.randn(self.batch_size, 1, self.num_patches_per_image, self.embedding_dim)
+        )
+
+        
+        self.encoders = nn.ModuleList([
+            nn.TransformerEncoder(
+                encoder_layer = nn.TransformerEncoderLayer(
+                    d_model = self.embedding_dim, # Embedding dimension of input
+                    nhead = 4,                    # Number of attention heads
+                    dim_feedforward = 128,
+                    dropout = 0.01,
+                    batch_first = True,
+                ),
+                num_layers = 4
+            )
+            for _ in range(num_modules)
+        ])
+
+        self.num_cross_layers = 4
+        self.cross_encoder = nn.ModuleList([
+            nn.MultiheadAttention(embed_dim = self.embedding_dim,
+                                  num_heads = 4,
+                                  batch_first = True
+            )
+            for _ in range(self.num_cross_layers)
+        ])
+
+        self.decoder = nn.Sequential(
+            nn.Conv2d(self.embedding_dim, self.embedding_dim // 2,
+                      kernel_size=3,
+                      padding=1),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=self.patch_size,
+                        mode="bilinear",
+                        align_corners=False),
+            nn.Conv2d(self.embedding_dim // 2, 1, kernel_size=1)
+        )
 
 
+    def forward(self, x):
+        x = x.unfold(dimension = 2, size = self.patch_size, step = self.patch_size) \
+             .unfold(dimension = 3, size = self.patch_size, step = self.patch_size)
+
+        x = x.contiguous().view(
+            self.batch_size,
+            self.num_modules,
+            self.num_patches_per_image,
+            self.patch_size * self.patch_size
+        )
+
+        x = self.patch_embedding(x)
+
+        x = x + self.pos_encoding
+
+        # Self-Attention
+        encoded_x = []
+        for i, encoder in enumerate(self.encoders):
+            encoded_x.append(encoder(x[:, i]))
+        x = torch.stack(encoded_x, dim = 1)
+
+        # Cross-Attention
+        mod0_tokens = x[:, 0, :, :]
+        mod1_tokens = x[:, 1, :, :]
+
+        for i, crossattention in enumerate(self.cross_encoder):
+            attn_out, _ = crossattention(query = mod0_tokens, # 0 queries 1
+                                            key = mod1_tokens,
+                                            value = mod1_tokens)
+            
+            mod0_tokens = mod0_tokens + attn_out
 
 
-    def feedforward(self):
-        pass
+        x = torch.stack([mod0_tokens, mod1_tokens], dim = 1)
 
-#device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
-#print(f"Using {device} device")
+        x = x[:, 0, :, :] # Isolating module 0 for prediction head
 
-model = STViT()#.to(device)
+        x = x.view(self.batch_size,
+                   self.img_height // self.patch_size, self.img_width // self.patch_size,
+                   self.embedding_dim).permute(0, 3, 1, 2)
 
-print(model)
+        # Decode
+        x = self.decoder(x).squeeze(1)
+
+        return x
--- code/main.py
+++ code/main.py
@@ -1,122 +1,37 @@
 #Internal
+import torch
 from ViT import STViT
 
-#External
-from time import time
-import torch
-import torch.nn as nn
-
-EMBED_DIM = 32
-
-batch_size = 1
-num_modules = 2
-height = 128
-width = 128
-
-print(f'Processing {2} input modules at dimensions {height}x{width} ')
-
-patch_size = 8 # Rectangular, number of pixels
+if __name__ == '__main__':
 
 
-num_patches_per_image = int((height / patch_size) * (width / patch_size))
-print(f'Patching images into {num_patches_per_image} patches with patch size: {patch_size}')
+    # Toy parameters
+    EMBED_DIM = 32
+    batch_size = 1
+    num_modules = 2
+    height = 128
+    width = 128
+    patch_size = 8 # Rectangular, number of pixels
 
-input_data = torch.randn(batch_size, num_modules, height, width)
+    # Define Model
+    model = STViT(batch_size = batch_size,
+                  num_modules = num_modules,
+                  img_height = height,
+                  img_width = width,
+                  patch_size = patch_size,
+                  embedding_dim = EMBED_DIM)#.to(device)
 
-target = torch.ones(batch_size, 1, height, width)
+    #print(model)
 
-#print(input_data)
-#print(input_data.shape)
+    # Some toy data
+    input_data = torch.randn(batch_size, num_modules, height, width)
+    target = torch.ones(batch_size, 1, height, width)
 
+    #Feedforward
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    print(f"Using {device} device")
+    model(input_data).to(device)
 
-# Patch transform
-patches = input_data.unfold(dimension = 2, size = patch_size, step = patch_size) \
-                    .unfold(dimension = 3, size = patch_size, step = patch_size)
-
-patches = patches.contiguous().view(
-    batch_size,
-    num_modules,
-    num_patches_per_image,
-    patch_size * patch_size
-)
-
-#print(patches)
-#print(patches.shape)
-
-# Patch embedding (Pixels --> Tokens)
-
-patch_embed = nn.Linear(patch_size*patch_size, EMBED_DIM)
-tokens = patch_embed(patches)
-
-#print(tokens)
-#print(tokens.shape)
-
-# Positional Encoding
-pos_embed = nn.Parameter(
-    torch.randn(batch_size, 1, num_patches_per_image, EMBED_DIM)
-)
-
-tokens = tokens + pos_embed # Add embedding, addition is automatically broadcasted correctly
-
-#print(tokens)
-#print(tokens.shape)
-
-## Define Self-Attention block
-# Singler transformer layer
-encoder_layer = nn.TransformerEncoderLayer(
-    d_model = EMBED_DIM, # Embedding dimension of input
-    nhead = 4,           # Number of attention heads
-    dim_feedforward = 128,
-    dropout = 0.01,
-    batch_first = True,
-)
-
-# Blocks of transformer layers
-encoder = nn.TransformerEncoder(
-    encoder_layer = encoder_layer,
-    num_layers = 4
-)
-
-# Perform per-module, per-batch self-attention
-encoded_modules = []
-for module in range(num_modules):
-    mod_tokens = tokens[:, module, :, :] # (Batch, Patch, Embedding) - Slice along module
-    encoded_module = encoder(mod_tokens)
-    encoded_modules.append(encoded_module)
-
-encoded_tokens = torch.stack(encoded_modules, dim=1)
-
-
-
-## Define Cross-Attention block
-# Single Cross-Attention layer
-cross_attention = nn.MultiheadAttention(embed_dim = EMBED_DIM,
-                                        num_heads = 4,
-                                        batch_first = True)
-
-#Block of Cross-Attention layers
-num_cross_layers = 3
-cross_encoder = nn.ModuleList([cross_attention for _ in range(num_cross_layers)])
-
-mod0_tokens = encoded_tokens[:, 0, :, :]
-mod1_tokens = encoded_tokens[:, 1, :, :]
-
-mod0_tokens, _ = cross_attention(query = mod0_tokens, # 0 queries 1
-                                 key = mod1_tokens,
-                                 value = mod1_tokens)
-
-tokens = torch.stack([mod0_tokens, mod1_tokens], dim = 1)
-
-fused = tokens[:, 0, :, :] # Isolated for prediction
-
-feat = fused.view(batch_size, height // patch_size, width // patch_size, EMBED_DIM).permute(0, 3, 1, 2)
-
-decoder = nn.Sequential(
-    nn.Conv2d(EMBED_DIM, EMBED_DIM//2, kernel_size=3, padding=1),
-    nn.ReLU(),
-    nn.Upsample(scale_factor=patch_size, mode="bilinear", align_corners=False),
-    nn.Conv2d(EMBED_DIM//2, 1, kernel_size=1)
-)
-
-head = decoder(feat).squeeze(1)
-print(head)
+    # Model params
+    #for name, param in model.named_parameters():
+    #    print(name, param.shape)
