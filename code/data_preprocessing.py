78477d52daa528db9f3e2c4017f43840941cac44
Carlo <carlo@familiecordes.de>
2026-03-16 16:14:30 +0100

    Completed duckdb workflow

--- code/data_preprocessing.py
+++ code/data_preprocessing.py
@@ -4,6 +4,7 @@
 import math
 
 import numpy as np
+from shapely import from_wkb
 import ee
 import geemap
 from pathlib import Path
@@ -22,6 +23,8 @@ import duckdb
 PROJECT_ID = 'transformerwildfire'
 #ee.Authenticate()
 ee.Initialize(project=PROJECT_ID)
+
+db_path = Path('data', 'raw', 'burn', 'burn_points.db')
 # ------------------------------ #
 
 
@@ -29,13 +32,22 @@ class GoldenGrid():
     """
     Defines project-unified grid via CRS, scale (m) and area of interest
     """
-    def __init__(self, crs, scale, bbox : list[float]):
+    def __init__(self, crs, scale, bbox : list[float],
+                start_date : str, end_date : str, day_interval: int):
         self.crs = crs
         self.scale = scale
         self.bbox = bbox
         self.aoi = ee.Geometry.Rectangle(bbox)
 
         self.target_proj = ee.Projection(crs).atScale(scale)
+        
+        self.start_date = start_date
+        self.end_date = end_date
+
+        self.day_interval = day_interval
+        self.dates = pd.date_range(start = start_date,
+                              end = end_date,
+                              freq = f'{day_interval}D')
 
 
 def get_one_lst_image(date, golden_grid : GoldenGrid):
@@ -120,16 +132,12 @@ def get_one_dtm_image(dtm_dir : Path, golden_grid : GoldenGrid) -> None:
         file_per_band=False
     )
 
-def ingest_burn_records(path_to_csv : Path,
-                        out_path : Path,
-                        golden_grid : GoldenGrid,
-                        day_interval : str
-                        ) -> None:
-    # DB config
-    db_name = 'burn_points.db'
+def ingest_burn_records(path_to_csv : Path) -> None:
     table_name = 'burn_points'
+    db_name = 'burn_points.db'
+    # DB config
     con = duckdb.connect(path_to_csv / db_name)
-    con.execute("INSTALL spatial;" \
+    con.execute("INSTALL spatial;"
                 "LOAD spatial;")
     con.execute(f"DROP TABLE IF EXISTS {table_name}")
     first = True
@@ -150,13 +158,17 @@ def ingest_burn_records(path_to_csv : Path,
         points["year"] = points["acq_date"].dt.year
         points["month"] = points["acq_date"].dt.month
         points["day_of_year"] = points["acq_date"].dt.dayofyear
+
+
         points["geometry"] = gpd.points_from_xy(points['longitude'], points['latitude'])
         points = points.drop(columns = ['latitude', 'longitude'])
         points = gpd.GeoDataFrame(
             points,
             crs="EPSG:4326",
         )
-        points = points.set_geometry('geometry').to_wkb() # Write to wkb for spatial extension usability
+
+        points["geom_wkb"] = points.geometry.to_wkb()
+        points = points.drop(columns = "geometry")
 
 
         # Write to db
@@ -167,24 +179,64 @@ def ingest_burn_records(path_to_csv : Path,
         if first:
             # Create Mode
             con.execute(f"DROP TABLE IF EXISTS {table_name}")
-            rel.create(table_name)
+            
+            con.execute(
+                f"""
+                CREATE TABLE {table_name} AS
+                SELECT
+                    * EXCLUDE geom_wkb,
+                    ST_GeomFromWKB(geom_wkb) AS geom
+                FROM {view_name};
+                """
+            )
+
             first = False
         else:
             # Append mode
-            rel.insert_into(table_name)
+            con.execute(
+                f"""
+                INSERT INTO {table_name}
+                SELECT
+                    * EXCLUDE geom_wkb,
+                    ST_GeomFromWKB(geom_wkb) AS geom
+                FROM {view_name};
+                """
+            )
+
         
     count = con.execute(f"""
             SELECT COUNT(*) FROM {table_name}
-            """).fetchall()
-    print(f'Wrote {count[0][0]} to {table_name}')
+            """).fetchall()[0][0]
+    print(f'Wrote {count} to {table_name}')
 
-def grid_target_map():
-    """
+def grid_target_map(path_to_db : Path,
+                    out_path : Path,
+                    golden_grid : GoldenGrid
+                    ) -> None:
+    table_name = 'burn_points'
     # Produce tifs at day interval
-    dates = pd.date_range(start=f'2024-01-01', end=f'2024-12-31', freq=f'{day_interval}D') # TODO: either add to goldengrid or as function variable, dont hardcode
+    #dates = pd.date_range(start=f'2024-01-01', end=f'2024-12-31', freq=f'{day_interval}D') # TODO: either add to goldengrid or as function variable, dont hardcode
 
-    for start_date in dates:
-        end_date = start_date + pd.Timedelta(days = day_interval)
+    con = duckdb.connect(path_to_db)
+    con.execute("""
+        INSTALL spatial;
+        LOAD spatial;
+    """)
+    
+    points = con.execute(f"""
+        SELECT ST_AsWKB(geom) AS geom_wkb,
+        * EXCLUDE geom
+        FROM {table_name}
+        WHERE acq_date BETWEEN '{golden_grid.start_date}' AND '{golden_grid.end_date}'
+    """).fetch_df()
+
+    points["geometry"] = points["geom_wkb"].apply(lambda g: from_wkb(bytes(g)))
+
+    points = gpd.GeoDataFrame(points, geometry="geometry", crs="EPSG:4326")
+    points = points.drop(columns="geom_wkb")
+
+    for start_date in golden_grid.dates:
+        end_date = start_date + pd.Timedelta(days = golden_grid.day_interval)
 
         mask = (points['acq_date'] >= start_date) & (points['acq_date'] < end_date)
         subset_gdf = points.loc[mask].copy()
@@ -196,8 +248,6 @@ def grid_target_map():
                          date_str = date_str,
                          out_path  = out_path,
                          golden_grid = golden_grid)
-        
-        """
 
 def rasterize_points(points: gpd.GeoDataFrame,
                      date_str: str,
@@ -264,20 +314,30 @@ def rasterize_points(points: gpd.GeoDataFrame,
     print(f"Rasterized {len(points_proj)} points for {date_str} to {output_file.name}")
 
 
-# Define local golden grid
-latmin, longmin = 36.812, -9.490
-latmax, longmax = 42.2724, -6.0234
-
-portugal_ggrid = GoldenGrid(crs = 'EPSG:3763',
-                            scale = 1000,
-                            bbox = [longmin, latmin, longmax, latmax])
 
 if __name__ == '__main__':
 
-    ingest_burn_records(path_to_csv = Path('data', 'raw', 'burn'),
+    # Define local golden grid
+    latmin, longmin = 36.812, -9.490
+    latmax, longmax = 42.2724, -6.0234
+
+    start_date = '2024-01-01'
+    end_date = '2024-12-31'
+
+    portugal_ggrid = GoldenGrid(
+        crs = 'EPSG:3763',
+        scale = 1000,
+        bbox = [longmin, latmin, longmax, latmax],
+        start_date = start_date,
+        end_date = end_date,
+        day_interval = 7
+    )
+
+    #ingest_burn_records(path_to_csv = Path('data', 'raw', 'burn'))
+
+    grid_target_map(path_to_db = db_path,
                     out_path = Path('data', 'processed', 'burn'),
-                    golden_grid = portugal_ggrid,
-                    day_interval = 8)
+                    golden_grid = portugal_ggrid)
 
     #get_one_dtm_image(Path('data', 'slope'), portugal_ggrid)
     #download_yearly_lst(2024, portugal_ggrid)
