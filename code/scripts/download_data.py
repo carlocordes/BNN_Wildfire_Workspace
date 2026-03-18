6a85a4911728991d7fbdfb80cf76a921919f6c39
Carlo <carlo@familiecordes.de>
2026-03-18 09:33:16 +0100

    Moved files

---
+++ code/scripts/download_data.py
@@ -0,0 +1,42 @@
+# Internal
+from src.core.goldengrid import GoldenGrid
+from src.data.download.dtm import get_one_dtm_image
+from src.data.download.lst_modis import download_yearly_lst
+from src.data.download.burn_targets import ingest_burn_records
+
+# External
+from pathlib import Path
+
+def main():
+    # Define local golden grid
+    latmin, longmin = 36.812, -9.490
+    latmax, longmax = 42.2724, -6.0234
+
+    start_date = '2024-01-01'
+    end_date = '2024-12-31'
+
+    portugal_ggrid = GoldenGrid(
+        crs = 'EPSG:3763',
+        scale = 1000,
+        bbox = [longmin, latmin, longmax, latmax],
+        start_date = start_date,
+        end_date = end_date,
+        day_interval = 7
+    )
+
+    # MODIS LST
+    #download_yearly_lst(2025, portugal_ggrid)
+    #download_yearly_lst(2024, portugal_ggrid)
+    #download_yearly_lst(2023, portugal_ggrid)    
+    #download_yearly_lst(2022, portugal_ggrid)
+
+    # DTM
+    get_one_dtm_image(Path('data', 'raw' ' dtm'), portugal_ggrid)
+
+    # Targets
+    ingest_burn_records(Path('data', 'raw', 'burn'))
+
+
+
+if __name__ == '__main__':
+    main()
