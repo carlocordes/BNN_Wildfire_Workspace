8619f50aa3e5ca5bb6d3ea288f4f63f6ca4bd77f
Carlo <carlo@familiecordes.de>
2026-03-23 15:59:37 +0100

    Config: create_dataset & train

--- code/te.py
+++ code/te.py
@@ -2,16 +2,11 @@
 Random off-topic trial error script
 """
 
-import pandas as pd
-from data_preprocessing import GoldenGrid
+from omegaconf import OmegaConf
+from pathlib import Path
 
-latmin, longmin = 36.812, -9.490
-latmax, longmax = 42.2724, -6.0234
+cfg = OmegaConf.load("configs/project.yaml")
 
-dr = pd.date_range(start = '2024-01-01', end = '2024-12-31', freq ='1D')
+path = cfg['data_sets']['path']
 
-portugal_ggrid = GoldenGrid(crs = 'EPSG:3763',
-                            scale = 1000,
-                            bbox = [longmin, latmin, longmax, latmax],
-                            start_date='2024-01-01', end_date='2024-12-31',
-                            day_interval=1)
+print(Path(path))
