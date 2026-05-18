1f826343895dd79129b113602de2d45460ce0b92
Carlo <carlo@familiecordes.de>
2026-05-18 16:23:35 +0200

    Run as gpu fix

--- scripts/train.py
+++ scripts/train.py
@@ -306,7 +306,7 @@ def main(config_path: Path, dataset_path: str, experiment_path: Path):
     cfg_training = cfg["training"]
     logging.info(f'Using config file from: {config_path}')
 
-    device = 'cpu' #torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
     logging.info(f"Using {device} device")
 
 
